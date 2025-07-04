a4ba11eadc318a4cdf7aff40ad037460601d4ede
hyeonguk <shyunguk126@naver.com>
2025-07-04 12:19:58 +0900

    entity ㅈ작성 완료

---
+++ attendance-service/src/main/java/com/playdata/attendanceservice/attendance/entity/VacationStatus.java
@@ -0,0 +1,7 @@
+package com.playdata.attendanceservice.attendance.entity;
+
+public enum VacationStatus {
+    REQUESTED, // 요청됨
+    APPROVED,  // 승인됨
+    REJECTED   // 반려됨
+}
---
+++ attendance-service/src/main/java/com/playdata/attendanceservice/attendance/entity/VacationType.java
@@ -0,0 +1,7 @@
+package com.playdata.attendanceservice.attendance.entity;
+
+public enum VacationType {
+    ANNUAL_LEAVE, // 연차
+    HALF_DAY_LEAVE // 반차
+}
+
