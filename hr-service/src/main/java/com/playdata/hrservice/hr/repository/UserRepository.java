63ab39f7f8f4c44e99b44fcd8a94871b5286c7c5
hoyeong-i <ghdud939@gmail.com>
2025-07-08 17:17:00 +0900

    더미 로직 개선, 쪽지 작업중

--- hr-service/src/main/java/com/playdata/hrservice/hr/repository/UserRepository.java
+++ hr-service/src/main/java/com/playdata/hrservice/hr/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.playdata.hrservice.hr.repository;
 
 
 import com.playdata.hrservice.hr.entity.User;
+import org.springframework.data.domain.Page; // 추가
+import org.springframework.data.domain.Pageable; // 추가
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +16,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmployeeNo(Long employeeNo);
+
+    // 사용자 검색을 위한 메서드 추가 (페이징 적용)
+    Page<User> findByUserNameContaining(String userName, Pageable pageable);
+
+    @Query("SELECT u FROM User u JOIN u.department d WHERE d.name LIKE %:departmentName%")
+    Page<User> findByDepartmentNameContaining(String departmentName, Pageable pageable);
+
+    @Query("SELECT u FROM User u JOIN u.department d WHERE u.userName LIKE %:userName% AND d.name LIKE %:departmentName%")
+    Page<User> findByUserNameContainingAndDepartmentNameContaining(String userName, String departmentName, Pageable pageable);
 }
