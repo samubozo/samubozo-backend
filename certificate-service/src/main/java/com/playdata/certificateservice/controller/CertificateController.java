6e3c0e26f0f1543b9cb467cfed7c14881605008d
yeeun-yenny <yeeun6328@gmail.com>
2025-07-14 12:27:43 +0900

    증명서 더미 데이터 추가

--- certificate-service/src/main/java/com/playdata/certificateservice/controller/CertificateController.java
+++ certificate-service/src/main/java/com/playdata/certificateservice/controller/CertificateController.java
@@ -30,7 +30,7 @@ public class CertificateController {
     }
 
     // 증명서 조회
-    @GetMapping("/certificate/list")
+    @GetMapping("/list")
     public ResponseEntity<?> listCertificates(@PageableDefault(size = 5, sort = "certificateId") Pageable pageable) {
         return new ResponseEntity<>(new CommonResDto(HttpStatus.OK,
                 "Success", certificateService.listCertificates(pageable)), HttpStatus.OK);
