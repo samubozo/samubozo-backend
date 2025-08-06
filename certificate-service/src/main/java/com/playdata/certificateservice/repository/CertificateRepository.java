37baed449d2fc2053c24d2418c230983cf8969eb
hyeonguk <shyunguk126@naver.com>
2025-08-07 02:54:23 +0900

    feat: 증명서 만료 상태 자동 업데이트 스케줄러 구현

--- certificate-service/src/main/java/com/playdata/certificateservice/repository/CertificateRepository.java
+++ certificate-service/src/main/java/com/playdata/certificateservice/repository/CertificateRepository.java
@@ -5,6 +5,7 @@ import com.playdata.certificateservice.entity.Certificate;
 import com.playdata.certificateservice.entity.Status;
 import com.playdata.certificateservice.entity.Type;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,4 +16,6 @@ public interface CertificateRepository extends JpaRepository<Certificate, Long> 
     Page<Certificate> findByEmployeeNo(Long employeeNo, Pageable pageable);
 
     Optional<Certificate> findByEmployeeNoAndTypeAndStatusAndExpirationDateAfter(Long employeeNo, Type type, Status status, LocalDate currentDate);
+
+    List<Certificate> findByExpirationDateBeforeAndStatus(LocalDate expirationDate, Status status);
 }
