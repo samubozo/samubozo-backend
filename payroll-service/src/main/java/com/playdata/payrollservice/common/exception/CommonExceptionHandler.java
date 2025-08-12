d5d44d46fa82f4f07cb6befa1481c08d63c1072e
Hoyoung Lee <99785007+hoyoungleee@users.noreply.github.com>
2025-08-13 00:18:56 +0900

    값없을때 에러탐지하게 화면이 되어있어서 에러 유발로 바꿈
    값없을때 에러탐지하게 화면이 되어있어서 에러 유발로 바꿈

--- payroll-service/src/main/java/com/playdata/payrollservice/common/exception/CommonExceptionHandler.java
+++ payroll-service/src/main/java/com/playdata/payrollservice/common/exception/CommonExceptionHandler.java
@@ -26,8 +26,8 @@ public class CommonExceptionHandler {
     public ResponseEntity<?> entityNotFountHandler(EntityNotFoundException e) {
         e.printStackTrace();
         CommonErrorDto errorDto
-                = new CommonErrorDto(HttpStatus.NO_CONTENT, e.getMessage());
-        return new ResponseEntity<>(errorDto, HttpStatus.NO_CONTENT);
+                = new CommonErrorDto(HttpStatus.NOT_FOUND, e.getMessage());
+        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
     }
 
     // 특정 권한을 가지지 못한 사용자가 요청을 보냈을 때 내쫓는 메서드
